ff10e83a035148dc68c72e971a8c133e655c558e
陈同学 <c_l-m@qq.com>
2025-01-09 19:33:41 +0800

    bg_mc_sdk-3.2.0.58572

--- sdk/mod/common/utils/mcmath.py
+++ sdk/mod/common/utils/mcmath.py
@@ -188,3 +188,107 @@ class Quaternion(object):
 
     def __str__(self):
         pass
+
+class Matrix(object):
+    # Creates a matrix of size numRows * numCols initialized to 0
+    def __init__(self, rowNum, colNum):
+        pass
+
+    # Create A Identity Matrix  of rowNum * rowNum
+    @staticmethod
+    def CreateEye(rowNum):
+        pass
+
+    @staticmethod
+    # Create A Matrix with data ,data should be int or float lists
+    def Create(data):
+        pass
+
+    @staticmethod
+    # Create a rotation matrix from euler, according to the sequence xyz sequence
+    def FromEulerXYZ(euler):
+        pass
+
+    @staticmethod
+    # Return back to euler from rotation matrix
+    def ToEulerXYZ(mat):
+        pass
+
+    # Set A Zero Matrix to Identity Matrix
+    def Eye(self):
+        pass
+
+    # Set Matrix Data with int or float lists [[]]
+    def SetData(self, data):
+        pass
+
+    def SetListData(self, data):
+        pass
+
+    # Create Matix with Quaternion Tuple (x,y,z,w)
+    @staticmethod
+    def QuaternionToMatrix(wxyz):
+        pass
+
+    def Copy(self):
+        pass
+
+    # Returns the number of rows in the matrix
+    @property
+    def row(self):
+        pass
+
+    # Returns the number of columns in the matrix
+    @property
+    def col(self):
+        pass
+
+    # Returns the value of element (i,j): x[i,j]
+    def __getitem__(self, ndxTuple):
+        pass
+
+    # Sets the value of element (i,j) to the value s: x[i,j] = s
+    def __setitem__(self, ndxTuple, value):
+        pass
+
+    def Transpose(self):
+        pass
+
+    def Inverse(self):
+        pass
+
+    # Decompose (T*R*S)Matrix to translate:(x,y,z) , quaternion:(x,y,z,w) ,scale:(x,y,z)
+    # Only When Matrix = T*R*S
+    # If Matrix = (T1*R1*S1)(T2*R2*S2) ,use DecomposeByQuaternion instead
+    def Decompose(self):
+        pass
+
+    # Decompose (T*R*S)Matrix with its quaternion to translate:(x,y,z)  ,scale:(x,y,z)
+    # When Matrix = T*R*S , quaterTuple = R.ToQuaternion().ToTuple()
+    # If Matrix = (T1*R1*S1)*(T2*R2*S2)*..., quaterTuple = R1.ToQuaternion()*R2.ToQuaternion()*...
+    def DecomposeByQuaternion(self, wxyz):
+        pass
+
+    # return  A Rotation Matrix to Quaternion (x,y,z,w)
+    def ToQuaternion(self):
+        # https://opensource.apple.com/source/WebCore/WebCore-514/platform/graphics/transforms/TransformationMatrix.cpp
+        pass
+
+    @staticmethod
+    def matrix4_multiply(lhs, rhs):
+        pass
+
+    # Creates and returns a new matrix that results from matrix addition
+    def __add__(self, rhsMatrix):
+        pass
+
+    # Creates and returns a new matrix resulting from matrix multiplcation
+    def __mul__(self, rhsMatrix):
+        pass
+
+    # Creates and returns a new matrix that results from matrix sub
+    def __sub__(self, rhsMatrix):
+        pass
+
+    def __str__(self):
+        pass
